b3362b11b64e2ae4e1af262896dec28b49767cc3
robinj97 <35749463+robinj97@users.noreply.github.com>
2022-06-21 21:15:31 +0200

    Parsing the url for queries
    Created logic to parse the URL to find which queries are being made and adding functionality to chain queries together.

--- src/main/java/kyc/App.java
+++ src/main/java/kyc/App.java
@@ -30,3 +30,56 @@ public final class App {
         }
 
         var serverOptions = new Options().withPort(port);
+
+    // Gets the query params after the /api/pep?
+    private static Map<String, String> parseQueryParams(String uri) {
+        var splitUri = uri.split("\\?");
+        if (splitUri.length < 2) {
+            return Map.of();
+        }
+
+        var queryParams = splitUri[1];
+        return Arrays.stream(queryParams.split("&"))
+                .map(paramPair -> paramPair.split("="))
+                .collect(Collectors.toMap(
+                        pair -> URLDecoder.decode(pair[0], StandardCharsets.UTF_8),
+                        pair -> URLDecoder.decode(pair[1], StandardCharsets.UTF_8))
+                );
+    }
+
+    private static Response handleRequest(List<PepDataRow> pepData, Request request) {
+        if (request.uri().startsWith("/api/pep")) {
+            var queryParams = parseQueryParams(request.uri());
+
+            if (queryParams.containsKey("name")) {
+                pepData = PepDataSearcher.searchByName(pepData, queryParams.get("name"));
+            }
+
+            if (queryParams.containsKey("dataset")) {
+                pepData = PepDataSearcher.searchByDataset(pepData, queryParams.get("dataset"));
+            }
+
+            var jsonData = Json.createArrayBuilder(pepData.stream().map(row -> row.toJson()).toList())
+                    .build();
+
+            var finalJson = Json.createObjectBuilder()
+                    .add("numberOfHits", pepData.size())
+                    .add("data", jsonData)
+                    .build();
+
+            return new Response(200, "OK", List.of(
+                    new Header("Content-Type", "application/json")
+            ), finalJson.toString().getBytes(StandardCharsets.UTF_8));
+        }
+
+        else {
+           return new Response(
+                   404,
+                   "OK",
+                   List.of(new Header("Content-Type", "application/json")),
+                   Json.createValue("Not Found")
+                           .toString()
+                           .getBytes(StandardCharsets.UTF_8));
+        }
+
+    }
